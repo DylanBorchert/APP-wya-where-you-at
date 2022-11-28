196362963ca97a684edda4edca2e2babbb8d9582
halna399 <halna399@mtroyal.ca>
2022-11-28 16:07:14 -0700

    adjusted friendslist to latest changes

--- WYA-app/FriendlistPage.js
+++ WYA-app/FriendlistPage.js
@@ -2,127 +2,18 @@
 // import React, { Component } from 'react';
 // import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, FlatList} from 'react-native';
 
-// const styles = StyleSheet.create({
-//     header:{
-//       backgroundColor: "#20B2AA",
-//     },
-//     headerContent:{
-//       padding:30,
-//       alignItems: 'center',
-//     },
-//     avatar: {
-//       width: 130,
-//       height: 130,
-//       borderRadius: 63,
-//       borderWidth: 4,
-//       borderColor: "#FFFFFF",
-//       marginBottom:10,
-//     },
-//     image:{
-//       width: 60,
-//       height: 60,
-//     },
-//     name:{
-//       fontSize:22,
-//       color:"#FFFFFF",
-//       fontWeight:'600',
-//     },
-//     body: {
-//       padding:30,
-//       backgroundColor :"#E6E6FA",
-//     },
-//     box: {
-//       padding:5,
-//       marginTop:5,
-//       marginBottom:5,
-//       backgroundColor: '#FFFFFF',
-//       flexDirection: 'row',
-//       shadowColor: 'black',
-//       shadowOpacity: .2,
-//       shadowOffset: {
-//         height:1,
-//         width:-2
-//       },
-//       elevation:2
-//     },
-//     username:{
-//       color: "#20B2AA",
-//       fontSize:22,
-//       alignSelf:'center',
-//       marginLeft:10
-//     }
-//   });
-
-
-// const Friendlist = () => {
-   
-
-// // export default class FriendsView extends Component {
-
-// //   constructor(props) {
-//     // super(props);
-//     // this.state = {
-//     //  const data: {
-//     //      {id:1, image: "https://bootdey.com/img/Content/avatar/avatar6.png", username:"johndoe1"},
-//     //      {id:2, image: "https://bootdey.com/img/Content/avatar/avatar2.png", username:"johndoe2"}
-//     //      {id:3, image: "https://bootdey.com/img/Content/avatar/avatar3.png", username:"johndoe3"},
-//     //      {id:4, image: "https://bootdey.com/img/Content/avatar/avatar4.png", username:"johndoe4"},
-//     //      {id:5, image: "https://bootdey.com/img/Content/avatar/avatar1.png", username:"johndoe5"},
-//     //      {id:6, image: "https://bootdey.com/img/Content/avatar/avatar6.png", username:"johndoe6"},
-// // };
-//     // };
-// //   }
-
-// //   render() {
-//     return (
-//       <View style={styles.container}>
-//           <View style={styles.header}>
-//             <View style={styles.headerContent}>
-//                 <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar1.png'}}/>
-//                 <Text style={styles.name}>John Doe</Text>
-//             </View>
-//           </View>
-
-//           <View style={styles.body}>
-//             <FlatList 
-//               style={styles.container} 
-//               enableEmptySections={true}
-//             //   data={this.state.data}
-//               keyExtractor= {(item) => {
-//                 return item.id;
-//               }}
-//               renderItem={({item}) => {
-//                 return (
-//                   <TouchableOpacity>
-//                     <View style={styles.box}>
-//                       <Image style={styles.image} source={{uri: item.image}}/>
-//                        <Text style={styles.username}>{item.username}</Text>
-//                     </View>
-//                   </TouchableOpacity>
-//                 )
-//             }}/>
-//           </View>
-//       </View>
-//     );
-//   }
-
-
-
-
-// // }
-
-
-// export default Friendlist;
 import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
   View,
   Image,
-  ScrollView,
+  Banner,
   TouchableOpacity,
   FlatList
 } from 'react-native';
+import { TouchableHighlight } from 'react-native-gesture-handler';
+
 
 export default class FriendlistPage extends Component {
 
@@ -130,26 +21,32 @@ export default class FriendlistPage extends Component {
     super(props);
     this.state = {
       data: [
-         {id:1, image: "https://bootdey.com/img/Content/avatar/avatar6.png", username:"johndoe1"},
-         {id:2, image: "https://bootdey.com/img/Content/avatar/avatar2.png", username:"johndoe2"},
-         {id:3, image: "https://bootdey.com/img/Content/avatar/avatar3.png", username:"johndoe3"},
-         {id:4, image: "https://bootdey.com/img/Content/avatar/avatar4.png", username:"johndoe4"},
-         {id:5, image: "https://bootdey.com/img/Content/avatar/avatar1.png", username:"johndoe5"},
-         {id:6, image: "https://bootdey.com/img/Content/avatar/avatar6.png", username:"johndoe6"},
+         {id:1, image: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000", username:"johndoe1"},
+         {id:2, image: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000", username:"johndoe2"},
+         {id:3, image: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000", username:"johndoe3"},
+         {id:4, image: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000", username:"johndoe4"},
+         {id:5, image: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000", username:"johndoe5"},
+         {id:6, image: "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000", username:"johndoe6"},
       ],
     };
   }
-
+  
+  
   render() {
+    const img = "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000";
+    const pressHandler = () => {
+      //goes to users classes "profile"
+      this.props.navigation.navigate('Classes');
+    }
     return (
-      <View style={styles.container}>
-          <View style={styles.header}>
-            <View style={styles.headerContent}>
-                <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar1.png'}}/>
-                <Text style={styles.name}>John Doe</Text>
-            </View>
-          </View>
 
+      <View style={styles.container}>
+        <View style={styles.banner}>
+          <Text style={styles.bannerText}>W Y A</Text>
+          <TouchableHighlight onPress={pressHandler}>
+          <Image style={styles.image} source={{uri:img}} /> 
+          </TouchableHighlight>
+        </View>
           <View style={styles.body}>
             <FlatList 
               style={styles.container} 
@@ -164,44 +61,49 @@ export default class FriendlistPage extends Component {
                     <View style={styles.box}>
                       <Image style={styles.image} source={{uri: item.image}}/>
                        <Text style={styles.username}>{item.username}</Text>
+                        <TouchableOpacity style={styles.button}>
+                         <Text style={styles.buttonText}>boop</Text>
+                        </TouchableOpacity>
                     </View>
                   </TouchableOpacity>
                 )
             }}/>
           </View>
       </View>
+      
     );
   }
 }
 
+
+
 const styles = StyleSheet.create({
-  header:{
-    backgroundColor: "#20B2AA",
-  },
-  headerContent:{
-    padding:30,
-    alignItems: 'center',
-  },
-  avatar: {
-    width: 130,
-    height: 130,
-    borderRadius: 63,
-    borderWidth: 4,
-    borderColor: "#FFFFFF",
-    marginBottom:10,
-  },
+  container: {
+    flex:1
+  }, 
+  banner: {
+    backgroundColor:'#5774ad',
+    padding: 3,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center', 
+    
+  }, 
+  bannerText: {
+    fontSize:30,
+    color: "#FFFFFF",
+    fontWeight: 'bold'
+  }
+  ,
   image:{
+    borderRadius:45,
     width: 60,
     height: 60,
   },
-  name:{
-    fontSize:22,
-    color:"#FFFFFF",
-    fontWeight:'600',
-  },
   body: {
+    flex:1,
     padding:30,
-    backgroundColor :"#E6E6FA",
+    backgroundColor :"#6d91d9",
   },
   box: {
     padding:5,
@@ -209,6 +111,7 @@ const styles = StyleSheet.create({
     marginBottom:5,
     backgroundColor: '#FFFFFF',
     flexDirection: 'row',
+    borderRadius: 15,
     shadowColor: 'black',
     shadowOpacity: .2,
     shadowOffset: {
@@ -218,9 +121,22 @@ const styles = StyleSheet.create({
     elevation:2
   },
   username:{
-    color: "#20B2AA",
+    color: "#ff9d99",
     fontSize:22,
     alignSelf:'center',
     marginLeft:10
+  }, 
+  button: {
+    alignSelf:'center',
+    marginLeft: 110, 
+    backgroundColor: '#6d91d9',
+    borderRadius:10
+  }, 
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize:17,
+    padding: 10,
+    fontWeight: 'bold',
+    
   }
 });
